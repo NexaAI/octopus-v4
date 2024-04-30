dd90b69060564e2182305b3e48909adc41fcfb32
Zack Zhiyuan Li <zhiyuanli0718@gmail.com>
2024-04-30 07:08:22 +0000

    add functional token mapping and license

---
+++ utils.py
@@ -0,0 +1,19 @@
+functional_token_mapping = {
+  "<nexa_0>": "physics_gpt",
+  "<nexa_1>": "chemistry_gpt",
+  "<nexa_2>": "biology_gpt",
+  "<nexa_3>": "computer_science_gpt",
+  "<nexa_4>": "math_gpt",
+  "<nexa_5>": "electrical_engineering_gpt",
+  "<nexa_6>": "history_gpt",
+  "<nexa_7>": "philosophy_gpt",
+  "<nexa_8>": "law_gpt",
+  "<nexa_9>": "politics_gpt",
+  "<nexa_10>": "culture_gpt",
+  "<nexa_11>": "economics_gpt",
+  "<nexa_12>": "geography_gpt",
+  "<nexa_13>": "psychology_gpt",
+  "<nexa_14>": "business_gpt",
+  "<nexa_15>": "health_gpt",
+  "<nexa_16>": "general_gpt"
+}
