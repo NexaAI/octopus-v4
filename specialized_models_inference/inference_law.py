bf6b4e6213f141ae605d231e32e3a059762ab908
Zack Zhiyuan Li <zhiyuan.li1995@hotmail.com>
2024-05-04 06:13:16 +0000

    add subjects

---
+++ specialized_models_inference/inference_law.py
@@ -0,0 +1,41 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+def model(accelerator, model_name_or_id=model_id):
+    # Setting up the model with necessary parameters
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name_or_id,
+        device_map={"": accelerator.process_index} if accelerator else "auto",
+        torch_dtype="auto",
+        trust_remote_code=True,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_id)
+    return model, tokenizer
+
+
+def prompt_format(user_query):
+    system_prompt = "You are an expert in law, skilled in constitutional law, criminal law, corporate law, and international law."
+    llama_prompt_template = """[INST] <</SYS>>\n{system_prompt}\n<</SYS>>\n\n{user_query} [/INST]"""
+    return llama_prompt_template.format(system_prompt=system_prompt, user_query=user_query)
+
+
+def inference(prompt, subject, pipe, tokenizer):
+    formatted_prompt = prompt_format(prompt)
+    prompt_tokenized = tokenizer(formatted_prompt, return_tensors="pt").to("cuda")
+    output_tokenized = pipe.generate(
+        **prompt_tokenized,
+        max_length=2048,
+        temperature=1
+    )
+    # Decode generated tokens to string
+    answer = tokenizer.decode(output_tokenized[0], skip_special_tokens=True)
+    return answer[len(formatted_prompt) :]
+
+
+if __name__ == "__main__":
+    prompt = "Analyze the impact of recent changes in international law on global trade agreements."
+    subject = "law"
+    pipe, tokenizer = model(None, model_id)
+    response = inference(prompt, subject, pipe, tokenizer)
+    print(response)
